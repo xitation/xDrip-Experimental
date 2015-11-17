eeea4fd24779d9b9ec534965636cdc6103ba003b
jstevensog <jstevensog@gmail.com>
2015-11-17 16:21:09 +1100

    Modified SimpleImageEncoder and BgSparklineBuilder.
    Saving for now while I try a fresh go at SimpleImageEncoder.

--- app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/BgSparklineBuilder.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/BgSparklineBuilder.java
@@ -37,6 +37,7 @@ public class BgSparklineBuilder {
     private boolean showHighLine = false;
     private boolean showAxes = false;
     private boolean useSmallDots = true;
+    private boolean useTinyDots = false;
 
     public BgSparklineBuilder setStart(long start) {
         this.start = start / BgGraphBuilder.FUZZER;
@@ -99,10 +100,16 @@ public class BgSparklineBuilder {
         this.useSmallDots = useSmallDots;
         return this;
     }
+    public BgSparklineBuilder setTinyDots(boolean useTinyDots) {
+        this.useTinyDots = useTinyDots;
+        return this;
+    }
 
     public BgSparklineBuilder setSmallDots() {
         return this.setSmallDots(true);
     }
+
+    public BgSparklineBuilder setTinyDots() { return this.setTinyDots(true); }
 
     public BgSparklineBuilder setBgGraphBuilder(BgGraphBuilder bgGraphBuilder) {
         this.bgGraphBuilder = bgGraphBuilder;
@@ -171,6 +178,10 @@ public class BgSparklineBuilder {
         if (useSmallDots) {
             for(Line line: lines)
                 line.setPointRadius(2);
+        }
+        if (useTinyDots) {
+            for(Line line: lines)
+                line.setPointRadius(1);
         }
         LineChartData lineData = new LineChartData(lines);
         if (showAxes) {
